096aafdf3ff465638a055a57ce603741f43e8908
Sergei Malevannyi <bbserg@yandex.ru>
2022-09-28 14:29:52 +0300

    Lesson #4: Solution for Task #16

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -28,4 +28,24 @@ class TestuserGet(BaseCase):
         expected_keys = ["username", "email", "firstName", "lastName"]
         Assertions.assert_json_has_keys(response2, expected_keys)
 
+    def test_get_another_user_by_id_authorized(self):
+        data = {'email': 'vinkotov@example.com', 'password': '1234'}
+
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+        user_id_login_method = self.get_json_value(response1, "user_id")
+        another_user_id = '1'
+
+        response2 = requests.get(f"https://playground.learnqa.ru/api/user/{another_user_id}",
+                                            headers={"x-csrf-token": token},
+                                            cookies={"auth_sid": auth_sid})
+
+        not_expected_keys = ["email", "firstName", "lastName"]
+
+        assert user_id_login_method != another_user_id, "Another user id is equal to id of authorised user "
+        Assertions.assert_json_has_key(response2, "username")
+        Assertions.assert_json_has_not_keys(response2, not_expected_keys)
+
 
